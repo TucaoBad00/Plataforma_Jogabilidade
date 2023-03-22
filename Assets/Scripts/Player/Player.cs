a571ca93c4d9a157b8e3c3722834f7dacf664aec
TucaoBad00 <arthurbadotti@hotmail.com>
2023-03-22 14:00:24 -0300

    camera

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,17 +7,38 @@ public class Player : MonoBehaviour
     public Rigidbody2D corpo;
     public Vector2 velocidade;
     public float speed;
+    public float jumpForce;
+    public float run;
+    private float currentspeed;
     private void Update()
     {
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            currentspeed = run;
+        }
+        else
+        {
+            currentspeed = speed;
+        }
         if(Input.GetKey(KeyCode.LeftArrow))
         {
             //corpo.MovePosition(corpo.position - velocidade * Time.deltaTime);
-            corpo.velocity = new Vector2(-speed, corpo.velocity.y);
+            corpo.velocity = new Vector2(-currentspeed, corpo.velocity.y);
         }
         else if(Input.GetKey(KeyCode.RightArrow))
         {
             //corpo.MovePosition(corpo.position + velocidade * Time.deltaTime);
-            corpo.velocity = new Vector2(+speed, corpo.velocity.y);
+            corpo.velocity = new Vector2(+currentspeed, corpo.velocity.y);
+        }
+        
+        jump();
+    }
+    private void jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            corpo.velocity = Vector2.up * jumpForce;
         }
     }
 }
