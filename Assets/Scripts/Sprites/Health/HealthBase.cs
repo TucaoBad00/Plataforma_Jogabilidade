be9fa5c8a82da1abbb96a4a72c18f1c65787cfb1
TucaoBad00 <arthurbadotti@hotmail.com>
2023-03-22 18:49:37 -0300

    player life

---
+++ Assets/Scripts/Sprites/Health/HealthBase.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBase : MonoBehaviour
+{
+    public float startLife;
+    public float _currentLife;
+    public bool isDead = false;
+
+    private void Awake()
+    {
+        _currentLife = startLife;
+
+    }
+    public void Damage(float damage)
+    {
+        if(isDead) return;
+        _currentLife -= damage;
+        if(_currentLife <= 0)
+        {
+            Kill();
+        }
+    }
+    private void Init()
+    {
+        _currentLife = startLife;
+        isDead = false;
+    }
+    private void Kill()
+    {
+        isDead = true;
+        Destroy(gameObject);
+    }
+    public void Update()
+    {
+
+    }
+}
